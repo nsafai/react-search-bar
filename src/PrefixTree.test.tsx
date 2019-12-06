8a448e700bd326c0d78b2eeb892feb03ca0cecb7
Nicolai Safai <nicolaisafai@gmail.com>
2019-12-05 17:29:59 -0800

    added test file to be completed

---
+++ src/PrefixTree.test.tsx
@@ -0,0 +1,22 @@
+// import { createTree, findNode, traverse, complete, strings, contains } from "./PrefixTree";
+
+// const examples = ['clever', 'classic', 'test'];
+
+// const tree = createTree(examples);
+// console.log(tree); // works
+// assert size = 3
+
+// const completions = complete(tree, 'cl');
+// console.log("should be clever and classic:", completions);
+
+// const findsExistingNode = findNode(tree, 'cle');
+// console.log("should be an object:", findsExistingNode)
+
+// const findsNonExistantNode = findNode(tree, 'clepto');
+// console.log("should be null:", findsNonExistantNode)
+
+// const containsClever = contains(tree, 'clever');
+// console.log("should be true:", containsClever)
+
+// const doesNotContainClever = contains(tree, 'cleverr');
+// console.log("should be false:", doesNotContainClever)
